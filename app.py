0cbdacc0aeb117990c9bc053459275bd917ab622
Elena Guijo Jimenez <eguijim1@alu.upo.es>
2026-02-24 21:12:47 +0100

    Add sidebar for rent and return
    Added the respective input for each option

--- app.py
+++ app.py
@@ -23,3 +23,48 @@ with col1:
 with col2:    
     st.metric(label='Número de Espacios Disponibles', value = sum(data['num_docks_available']))
     st.metric(label='Estaciones con Espacios Disponibles', value = len(data[data['num_docks_available'] > 0]))
+
+# ---- SIDEBAR ----
+with st.sidebar:
+    option = st.selectbox(
+        '¿Quieres alquilar o devolver una bicicleta?',
+        ['Alquilar', 'Devolver']
+    )
+
+    # ALQUILAR OPTION
+    if option == 'Alquilar':
+        bike_type = st.multiselect(
+            '¿Qué tipo de bicicletas quieres alquilar?',
+            ['ICONIC', 'BOOST', 'FIT', 'EFIT']
+        )
+        st.header('¿Dónde te encuentras?')
+        street = st.text_input('Calle')
+        city = st.text_input('Ciudad', 'Barcelona')
+        country = st.text_input('País', 'España')
+        drive_selected = st.checkbox('Me dirijo hacia allí')
+
+        # FIND BIKES BUTTON
+        find_bikes_button = st.button('Buscar bicicletas', type='primary')
+        if find_bikes_button:
+            if street != "":
+                actual_location = geocode(street + " " + city + " " + country)
+                if actual_location == "":
+                    st.subheader('Dirección no válida.')    
+            else:
+                st.subheader('Dirección no válida')
+    # DEVOLVER OPTION
+    elif option == 'Devolver':
+        st.header('¿Dónde te encuentras?')
+        street = st.text_input('Calle')
+        city = st.text_input('Ciudad', 'Barcelona')
+        country = st.text_input('País', 'España')
+
+        # FIND DOCKS BUTTON
+        find_docks_button = st.button('Buscar espacios', type='primary')
+        if find_docks_button:
+            if street != "":
+                actual_location_return = geocode(street + " " + city + " " + country)
+                if actual_location_return == "":
+                    st.subheader('Dirección no válida')    
+            else:
+                st.subheader('Dirección no válida')
